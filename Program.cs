5e5368d7997e379b1a6346b4d7056448fac24c13
rufatgit <rufat.valiyev02@gmail.com>
2023-02-18 13:08:39 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿
+
+Console.WriteLine("Enter 2 numbers to perform operations.");
+double a=int.Parse(Console.ReadLine());
+double b = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Which operation?");
+char c=char.Parse(Console.ReadLine());
+
+
+if (c=='+')
+{
+    Console.WriteLine(a+b);
+}
+else if (c=='-')
+{
+    Console.WriteLine(a-b);
+}
+else if (c=='/')
+{
+    Console.WriteLine(a/b);
+}
+else if(c=='*' || c=='x')
+{
+    Console.WriteLine(a*b);
+}
+else
+{
+    Console.WriteLine("Wrong char");
+}
+
+
+
+
+
+
+
+
+
